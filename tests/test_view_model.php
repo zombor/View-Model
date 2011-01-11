6ec7493837ae38022222244e8ecdaf4eddf0813b
Jeremy Bush <contractfrombelow@gmail.com>
2011-01-11 06:57:12 -0600

    adding a basic test

---
+++ tests/test_view_model.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Tests View_Model functionality
+ *
+ * @group view_model
+ */
+class View_Model_Test extends PHPUnit_Framework_TestCase
+{
+	public function test_render()
+	{
+		$view = new View_Test();
+		
+		$expected = file_get_contents(Kohana::find_file('tests', 'output/test', 'txt'));
+		$this->assertSame($expected, $view->render());
+	}
+}
+
+class View_Test extends View_Model
+{
+	public function var_foo()
+	{
+		return '<h2>foobar</h2>';
+	}
+}
