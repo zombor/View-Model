d9ef935348a071cf7dbd0b7374503f01836a25ad
Jeremy Bush <contractfrombelow@gmail.com>
2011-01-11 07:04:41 -0600

    making test slightly better

--- tests/test_view_model.php
+++ tests/test_view_model.php
@@ -10,7 +10,7 @@ class View_Model_Test extends PHPUnit_Framework_TestCase
 	public function test_render()
 	{
 		$view = new View_Test();
-		
+
 		$expected = file_get_contents(Kohana::find_file('tests', 'output/test', 'txt'));
 		$this->assertSame($expected, $view->render());
 	}
